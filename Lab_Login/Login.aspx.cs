306ac5d0456cdf8b429bc9fe80d89cc6e290a20e
curtis567 <eveny15467@gmail.com>
2017-06-19 22:26:49 +0800

    hw Login

---
+++ Lab_Login/Login.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        if (txtUserName.Text.Trim() == "") {
+            lblPrompt.Text = "Plase key your Name";
+            return;
+        }
+        if (txtPassword.Text.Trim() == "") {
+            lblPrompt.Text = "Plase key your Passeord";
+            return;
+        }
+        Session["UserName"] = txtUserName.Text;
+        Response.Redirect("secret.aspx");
+        string s = Session["UserName"].ToString();
+        if (s == null) {
+            Response.Redirect("Default.aspx");
+        } else {
+            Response.Redirect(s);
+        }
+
+
+    }
+
+    protected void btnOut_Click(object sender, EventArgs e) {
+        Session.Abandon();
+    }
+}
---
+++ Lab_Login/Secret.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="Secret.aspx.cs" Inherits="Secret" Debug="true"%>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
+    <p>
+    Content for member only.</p>
+    <p>
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+    </p>
+<p>
+    <br />
+</p>
+<p>
+</p>
+</asp:Content>
+
